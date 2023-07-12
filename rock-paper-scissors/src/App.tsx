9d19b721ddedefdb983611df6439f3341df81e6c
mengzhuou <71995520+mengzhuou@users.noreply.github.com>
2023-07-11 21:44:27 -0400

    added random functionalities

--- rock-paper-scissors/src/App.tsx
+++ rock-paper-scissors/src/App.tsx
@@ -7,21 +7,35 @@ import Scissors from './Scissors.png';
 
 
 class App extends React.Component {
+  state = {player1Image: Rock, player2Image: Rock}
+
+  getRandomImage = () => {
+    const image = [Rock, Paper, Scissors];
+    const randomNum = Math.floor(Math.random() * 3);
+    return image[randomNum];
+  }
+
+  handleRandom1 = () => {
+    this.setState({ player1Image: this.getRandomImage() });
+  }
+
+  handleRandom2 = () => {
+    this.setState({ player2Image: this.getRandomImage() });
+  }
   render() {
+    const { player1Image, player2Image } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <div className="player-section" style={{ backgroundColor: 'blue' }}>
+          <div className="player1-section">
             <h2>Player 1</h2>
-            <img src={Rock} alt="Player 1" />
-            <button>Random</button>
+            <img src={player1Image} alt="Player 1" />
+            <button onClick={this.handleRandom1}>Random</button>
           </div>
-          <div className="player-section" style={{ backgroundColor: 'red' }}>
+          <div className="player2-section">
             <h2>Player 2</h2>
-            <img src={Rock} alt="Player 2" />
-            <button>Random</button>
+            <img src={player2Image} alt="Player 2" />
+            <button onClick={this.handleRandom2}>Random</button>
           </div>
-        </header>
       </div>
     );
   }
